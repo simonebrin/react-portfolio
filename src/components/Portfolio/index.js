d9cd906aa869a76a0eecfd9ed452b59b64a0b56a
Simone Brin <simonebrin@gmail.com>
2022-11-08 11:03:58 -0500

    started portfolio section

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,15 +1,45 @@
 import React, { useState } from 'react';
-import Project from '../Project'
+import Project from '../Project';
+import firstPhoto from '../../assets/portfolioImages/runbuddy.jpg';
+import secondPhoto from '../../assets/portfolioImages/emoji.png'
 
 const Portfolio = () => {
-    const [projects] = useState([{ name: 'project1', url: 'https://www.google.com', image: ''}, { name: 'project2', url: 'https://www.youtube.com' }, { name: 'project3', url: 'https://www.google.com'}])
-  return (
+    // const [projects] = useState([{ name: 'project1', url: 'https://www.google.com', image: '' }, { name: 'project2', url: 'https://www.youtube.com' }, { name: 'project3', url: 'https://www.google.com' }])
+    // const [projects] = useState([{
+    //      name: 'project1', photo: { firstPhoto },
+    // }]);
+    const [projects] = useState([{
+        id: '1',
+        image: firstPhoto,
+        name: 'project1',
+        url: 'https://www.google.com'
+    },
+        {
+        id: '2',
+        image: secondPhoto,
+        name: 'project2',
+        url: 'https://www.youtube.com'
+    }
+    ])
+return (
     <div>
       {projects.map((project) => (
-        <Project project={project} key={project.name} />
+          <Project project={project} key={project.name} img src={project.image} />
       ))}
     </div>
   );
 }
 
 export default Portfolio
+
+
+//   return (
+//     <div>
+//       {projects.map((e, project) => (
+//           <Project photo={e.photo}
+//               text={e.name}
+//               key={e.name} />
+//       ))}
+//     </div>
+//   );
+// }
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 const Project = ({project, key}) => {
   return (
-      <div key={key}>
-      <h3>{project.name}</h3>
-      <a href={project.url}>Here is a link</a>
+    <div key={key}>
+      <div className="head-text">
+        <div className="head-image">
+                  <img src={project.image} alt="project" />
+        </div>
+        <div className="text-on-image">
+          <h3>{project.name}</h3>
+          <a href={project.url}>Here is a link</a>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Project
+
+// const Project = ({ project, key }) => {
+//   return (
+//     <div key={key}>
+//       <img src={project.image} alt="project" />
+//       <h3>{project.name}</h3>
+//       <a href={project.url}>Here is a link</a>
+//     </div>
+//   );
+// };
