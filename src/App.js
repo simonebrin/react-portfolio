3addbdb509bf757e738a6a7bc47f12952e0e7a8d
Simone Brin <simonebrin@gmail.com>
2022-11-13 14:09:27 -0500

    work?

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import Nav from './components/Nav';
+// import Nav from './components/Nav';
 // import Footer from './components/Footer';
 // import Header from './components/Header';
 import Portfolio from './components/Portfolio';
